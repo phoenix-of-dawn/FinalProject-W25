2e4b78a229c47496cdb752c367fea814f1e5789d
phoenix-of-dawn <eliobenoit@live.com>
2025-04-29 13:38:58 -0400

    Make Module abstract class

---
+++ src/main/java/org/example/Module.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Comparator;
+
+public abstract class Module implements Comparable<Module> {
+    String name;
+    int difficulty;
+
+    abstract void display();
+
+    abstract boolean solve(int answer);
+
+    @Override
+    public int compareTo(Module o) {
+        return (o.difficulty - this.difficulty) * 1000 + (o.name.compareTo(this.name));
+    }
+
+    public static class ModuleComparator implements Comparator<Module> {
+        String compared;
+
+        public ModuleComparator(String compared) {
+            this.compared = compared;
+        }
+
+        @Override
+        public int compare(Module o1, Module o2) {
+            return switch (this.compared) {
+                case "name" -> o1.name.compareTo(o2.name);
+                case "difficulty" -> o2.difficulty - o1.difficulty;
+                case null, default -> o1.compareTo(o2);
+            };
+        }
+    }
+}
