27297821b65afaedebd029e98a12df7c5035ffd4
phoenix-of-dawn <eliobenoit@live.com>
2025-05-09 19:23:46 -0400

    Implement display + solve + randomize for Caeser Module

--- src/main/java/org/example/modules/CaeserModule.java
+++ src/main/java/org/example/modules/CaeserModule.java
@@ -1,6 +1,9 @@
 package org.example.modules;
 
+import org.example.GameController;
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class CaeserModule extends Module implements Randomizable {
     private int shift;
@@ -12,18 +15,7 @@ public class CaeserModule extends Module implements Randomizable {
         this.name = "Caeser";
         this.difficulty = 1;
 
-        Random rand = new Random();
-
-        this.shift = rand.nextInt(1,26);
-
-        for (int i = 0; i < 8; i++) {
-            decodedString += ALPHABET.charAt(rand.nextInt(0, 26));
-        }
-
-        for (int i = 0; i < 8; i++) {
-            char toAdd = (char) ((decodedString.charAt(i) + shift - 'a') % 26 + 'a');
-            caeserString += toAdd;
-        }
+        randomize();
     }
 
     /**
@@ -46,18 +38,43 @@ public class CaeserModule extends Module implements Randomizable {
 
     @Override
     public void display() {
-        // TODO: Implement this
+        System.out.println("You see a display with the word \"" + caeserString + "\" and the number " + shift);
+        System.out.println("Underneath it, there is a keyboard with the only the alphabet on it.");
+        System.out.println("What do you write?");
+
+        Scanner sc = new Scanner(System.in);
+        String ans = sc.next();
+        while (!ans.matches("[a-z]+")) {
+            System.out.println("Invalid input");
+            ans = sc.next();
+        }
+
+        if (!solve(ans)) {
+            GameController.getInstance().strike();
+            randomize();
+            display();
+        }
     }
 
     @Override
     public boolean solve(String answer) {
-        // TODO: Implement this
-        return false;
+        return answer.equals(decodedString);
     }
 
     @Override
     public void randomize() {
-        // TODO: Implement this
+        Random rand = new Random();
+
+        this.shift = rand.nextInt(1,26);
+
+        for (int i = 0; i < 8; i++) {
+            decodedString += ALPHABET.charAt(rand.nextInt(0, 26));
+        }
+
+        for (int i = 0; i < 8; i++) {
+            char toAdd = (char) ((decodedString.charAt(i) + shift - 'a') % 26 + 'a');
+            caeserString += toAdd;
+        }
     }
 
     public String getCaeserString() {
