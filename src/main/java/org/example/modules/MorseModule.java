c62d033e25571655609c628e05bfd6706b3fda29
phoenix-of-dawn <eliobenoit@live.com>
2025-05-09 19:57:10 -0400

    Implemented display and solve for MorseModule

--- src/main/java/org/example/modules/MorseModule.java
+++ src/main/java/org/example/modules/MorseModule.java
@@ -1,15 +1,62 @@
 package org.example.modules;
 
+import org.example.GameController;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 
 public class MorseModule extends Module {
     private String decodedString;
     private String code;
 
-    private static Map<Character, String> ALPHABET;
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final Map<Character, String> MORSE_TO_ALPHABET = new HashMap<Character, String>();
+
+    static {
+        MORSE_TO_ALPHABET.put('a', ".-");
+        MORSE_TO_ALPHABET.put('b', "-...");
+        MORSE_TO_ALPHABET.put('c', "-.-.");
+        MORSE_TO_ALPHABET.put('d', "-..");
+        MORSE_TO_ALPHABET.put('e', ".");
+        MORSE_TO_ALPHABET.put('f', "..-.");
+        MORSE_TO_ALPHABET.put('g', "--.");
+        MORSE_TO_ALPHABET.put('h', "....");
+        MORSE_TO_ALPHABET.put('i', "..");
+        MORSE_TO_ALPHABET.put('j', ".---");
+        MORSE_TO_ALPHABET.put('k', "-.-");
+        MORSE_TO_ALPHABET.put('l', ".-..");
+        MORSE_TO_ALPHABET.put('m', "--");
+        MORSE_TO_ALPHABET.put('n', "-.");
+        MORSE_TO_ALPHABET.put('o', "---");
+        MORSE_TO_ALPHABET.put('p', ".--.");
+        MORSE_TO_ALPHABET.put('q', "--.-");
+        MORSE_TO_ALPHABET.put('r', ".-.");
+        MORSE_TO_ALPHABET.put('s', "...");
+        MORSE_TO_ALPHABET.put('t', "-");
+        MORSE_TO_ALPHABET.put('u', "..-");
+        MORSE_TO_ALPHABET.put('v', "...-");
+        MORSE_TO_ALPHABET.put('w', ".--");
+        MORSE_TO_ALPHABET.put('x', "-..-");
+        MORSE_TO_ALPHABET.put('y', "-.--");
+        MORSE_TO_ALPHABET.put('z', "--..");
+    }
 
     public MorseModule() {
-        // TODO: Make this
+        this.name = "Morse";
+        this.difficulty = 5;
+
+        Random rand = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            this.decodedString += String.valueOf(ALPHABET.charAt(rand.nextInt(0, 26)));
+        }
+
+        for (int i = 0; i < decodedString.length() - 1; i++) {
+            this.code += MORSE_TO_ALPHABET.get(this.decodedString.charAt(i)) + " / ";
+        }
+        this.code += MORSE_TO_ALPHABET.get(this.decodedString.charAt(decodedString.length() - 1)) + " / ";
     }
 
     /**
@@ -27,13 +74,25 @@ public class MorseModule extends Module {
 
     @Override
     public void display() {
-        // TODO: Implement display for MorseModule
+        System.out.println("You see a display with \"" + code + "\" on it");
+        System.out.println("There is a keyboard with only the alphabet on it. What do you write on it?");
+
+        Scanner sc = new Scanner(System.in);
+        String ans = sc.next();
+        while (!ans.matches("[a-z]+")) {
+            System.out.println("Invalid input");
+            ans = sc.next();
+        }
+
+        if (!solve(ans)) {
+            GameController.getInstance().strike();
+            display();
+        }
     }
 
     @Override
     public boolean solve(String answer) {
-        // TODO: Implement solve for MorseModule
-        return false;
+        return answer.equals(decodedString);
     }
 
     public String getDecodedString() {
