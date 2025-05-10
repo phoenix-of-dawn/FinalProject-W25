2f04e78c387d1310e8a58060ad7802e9556b291b
phoenix-of-dawn <eliobenoit@live.com>
2025-05-09 21:15:15 -0400

    Implemented everything in GameController

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -3,11 +3,18 @@ package org.example;
 import org.example.modules.*;
 import org.example.modules.Module;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class GameController {
     private List<Module> modules;
     private int strikesLeft;
+    private long timeTaken;
+    private String playerName;
 
     private static GameController instance;
 
@@ -27,16 +34,36 @@ public class GameController {
         return instance;
     }
 
+    /**
+     * runs the game and handles all the outside logic
+     */
     public void run() {
+        System.out.println("Do you want to see previous runs or do a new run? 1 or 2");
+        Scanner sc = new Scanner(System.in);
+        String in = sc.next();
+        while (!in.equals("1") && !in.equals("2")) {
+            System.out.println("Invalid input");
+            in = sc.next();
+        }
+
+        if (in.equals("1")) {
+            showRuns();
+            return;
+        }
+
         System.out.println("You have a bomb with modules: ");
         for (Module module : modules) {
             System.out.println(module.getName());
         }
 
+        System.out.println("Your time starts now!");
+        long startTime = System.currentTimeMillis();
+
         for (Module module : modules) {
             module.display();
             if (strikesLeft <= 0) {
                 System.out.println("The bomb has exploded. You have lost");
+                timeTaken = System.currentTimeMillis() - startTime;
                 saveRun();
                 return;
             }
@@ -46,10 +73,16 @@ public class GameController {
         }
     }
 
+    /**
+     * when someone fails at a module, remove a strike
+     */
     public void strike() {
         strikesLeft--;
     }
 
+    /**
+     * initializes the modules array list
+     */
     private void initModules() {
         this.modules = new ArrayList<>();
         Set<Integer> seen = new HashSet<>();
@@ -77,8 +110,49 @@ public class GameController {
         this.modules.sort(new Module.ModuleComparator("difficulty"));
     }
 
+    /**
+     * saves a run to a csv file
+     */
     private void saveRun() {
-        // TODO: Implement this
+        File runs = new File("src/main/resources/runs.csv");
+
+        try (FileWriter fileWriter = new FileWriter(runs, true)) {
+            String toWrite = "";
+            toWrite += playerName + ",";
+            toWrite += (strikesLeft >= 0) + ",";
+            toWrite += strikesLeft + ",";
+            toWrite += timeTaken + "\n";
+            fileWriter.write(toWrite);
+        } catch (IOException e) {
+            throw new RuntimeException("File not writable: ", e);
+        }
+    }
+
+    /**
+     * displays all the runs in order of completion
+     */
+    private void showRuns() {
+        File file = new File("src/main/resources/runs.csv");
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNextLine()) {
+                String runString = sc.nextLine();
+                if (runString == null || runString.isBlank()) {
+                    return;
+                }
+
+                String[] run = runString.split(",");
+                System.out.printf("Name: %s. Bomb defused: %b. Strikes left: %s. Time taken (in seconds): %d",
+                        run[0],
+                        Boolean.getBoolean(run[1]),
+                        run[2],
+
+                        // Is in milliseconds, need to divide by 1000 to get seconds
+                        Long.getLong(run[3]) / 1000
+                );
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No runs completed yet.");
+        }
     }
 
     public List<Module> getModules() {
